f7a085cbdf56470e632edda42991903083709d20
ssebass1045 <sebas.22.26@hotmail.com>
2024-05-05 14:43:29 -0500

    homeworck 02 completada

---
+++ back/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var num1 = 7;
+var num2 = 5;
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(num1, num2));
---
+++ back/src/index.ts
@@ -0,0 +1,8 @@
+const num1: number = 7;
+const num2: number = 5;
+
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+console.log(sum(num1, num2));
