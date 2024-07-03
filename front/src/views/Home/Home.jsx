2ebb7641f51be1eef8d64b4bb34fd78227b920c7
ssebass1045 <sebas.22.26@hotmail.com>
2024-07-03 10:41:35 -0500

    homework React 01

---
+++ front/src/views/Home/Home.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../../components/Navbar/Navbar";
+
+Navbar
+const Home = () => {
+
+    return (
+        <div>
+            
+            <Navbar/>
+            <h1>Home</h1>
+        </div>
+    )
+
+};
+
+export default Home;
